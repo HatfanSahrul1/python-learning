181aed66d876056c31bca7ac3d6848cab60b58bf
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-03-30 17:28:14 +0700

    update : add swap variable method

--- simpleOperator.py
+++ simpleOperator.py
@@ -15,7 +15,12 @@ class SimpleMath:
         return a * b
     
     def Bagi(self, a, b):
+        # ternary operation here
         return 0 if b==0 else a / b
+    
+    def Tukar(self, a, b):
+        a, b = b, a
+        return a, b
     
 
 def main():
@@ -25,8 +30,12 @@ def main():
     bil2 = 27
 
     hasil = math.Jumlah(bil1, bil2)
-
     print(hasil)
+
+    bil1, bil2, = math.Tukar(bil1, bil2)
+
+    print(bil1)
+    print(bil2)
 
     return 0
 
