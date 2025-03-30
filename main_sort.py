f72b6e0fa55f407e07ff9d41d2d24cf02fea7478
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-03-31 01:34:12 +0700

    update : learn - create simple reference from other file - simple array - simple for loop - simple manual buble sort

--- main_sort.py
+++ main_sort.py
@@ -1,11 +1,18 @@
 from simpleOperator import SimpleMath
 
 def main():
+    math = SimpleMath()
 
     arr = [12, 9, 2, 4, 5, 1, 0, 89, 32, 54, 6]
 
-    for x in arr:
-        print(x)
+    for x in range(0, len(arr)):
+        for y in range(x+1, len(arr)):
+            if arr[x] > arr[y] :
+                arr[x], arr[y] = math.Tukar(arr[x], arr[y])
+
+    for n in arr:
+        print(n)
+
 
 
 if __name__ == '__main__':
