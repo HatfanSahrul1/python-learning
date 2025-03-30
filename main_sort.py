dba5325198c15378169189d973ce1eb3bab426ae
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-03-30 18:21:50 +0700

    update : learn array and for

---
+++ main_sort.py
@@ -0,0 +1,14 @@
+from simpleOperator import SimpleMath
+
+def main():
+
+    arr = [12, 9, 2, 4, 5, 1, 0, 89, 32, 54, 6]
+
+    for x in arr:
+        print(x)
+
+
+if __name__ == '__main__':
+    main()
+
+    
