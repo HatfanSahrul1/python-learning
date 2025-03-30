e93eb19402cc4ecd75d2e7b440acb509c72bec3f
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-03-31 01:50:25 +0700

    update : add new method for manual buble sort

--- simpleOperator.py
+++ simpleOperator.py
@@ -21,3 +21,11 @@ class SimpleMath:
     def Tukar(self, a, b):
         a, b = b, a
         return a, b
+    
+    def BubbleSort(self, arr):
+        for x in range(0, len(arr)):
+            for y in range(x+1, len(arr)):
+                if arr[x] > arr[y] :
+                    arr[x], arr[y] = self.Tukar(arr[x], arr[y])
+        
+        return arr
