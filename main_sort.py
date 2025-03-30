b4a3668723236f2ad31292bc47fad872e12d3dd6
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-03-31 01:50:42 +0700

    update : implement method form SimpleMath

--- main_sort.py
+++ main_sort.py
@@ -5,10 +5,7 @@ def main():
 
     arr = [12, 9, 2, 4, 5, 1, 0, 89, 32, 54, 6]
 
-    for x in range(0, len(arr)):
-        for y in range(x+1, len(arr)):
-            if arr[x] > arr[y] :
-                arr[x], arr[y] = math.Tukar(arr[x], arr[y])
+    arr = math.BubbleSort(arr)
 
     for n in arr:
         print(n)
